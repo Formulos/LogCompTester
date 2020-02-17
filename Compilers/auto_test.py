41812b02da40f27ffd118e34d20694522258ce9b
formulos <paulotozp@gmail.com>
2020-02-17 11:23:19 -0300

    suporta numeros negativos e c++ roda

--- Compilers/auto_test.py
+++ Compilers/auto_test.py
@@ -7,7 +7,7 @@ import json
 
 
 
-def test_main(person,DIR,language,args):
+def test_main(person,DIR,language,args,compile_args):
     
     size_test = (len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])) //2
     src_file = "src/{!s}".format(person)
@@ -17,6 +17,16 @@ def test_main(person,DIR,language,args):
     failed_test = False
 
     args.append("")
+
+    if language != "python3": #refatorar esse codigo, dois checks iguais
+        output = compile(src_file,compile_args)
+
+        if output: #strings vazias são falsas
+            report += "Error: teste automatico não conseguio compilar arquivo!\n"
+            report += "parametros de compilação: {!s}\n".format(" ".join(compile_args))
+            report +=  "erro de compilação:{!s}".format(output)
+            report_writer(report,person)
+            return True
 
     for i in range(1,size_test + 1):
         test_file = DIR +"/teste{!s}.txt".format(i)
@@ -31,7 +41,7 @@ def test_main(person,DIR,language,args):
         if output == "":
             report += "Error: main file not found\n"
             report_writer(report,person)
-            print(output)
+            #print(output)
             return True
 
         sol = get_text(sol_file)
@@ -39,14 +49,19 @@ def test_main(person,DIR,language,args):
 
         first_digit = re.search(r"\d", output) #lida com texto aleatorio das versão 1.0
         first_digit = first_digit.start()
+
+        if output[first_digit-1] == "-": # não se se tem jeito melhor que esse
+            first_digit -= 1 # feito para não iginorar numeros negativos
+
         output = output[first_digit:]
+        output = text_processor(output)
 
         result = assertEquals(sol, output)
         if result:
             report += "teste{!s}: ok\n \n".format(str(i))
         else:
             report += "teste{!s}: falha\n".format(str(i))
-            report += "input do teste: {!s}".format(str(data))
+            report += "input do teste: {!s} ".format(str(data))
             report += "output esperado: {!s} | output recebido:{!s}\n \n".format(str(sol),str(output))
             failed_test = True
 
@@ -74,6 +89,12 @@ def get_program_output(src_file,language,args):
 
     return text
 
+def compile(src_file,compile_args):
+    output = subprocess.run(args,cwd=src_file,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+    text = output.stderr.decode("utf-8")
+
+    return text
+
 def text_processor(text):
     text = os.linesep.join([s for s in text.splitlines() if s])
     text.strip()
@@ -93,7 +114,7 @@ def read_git_url_json():
 
 if __name__ == '__main__':
     test_dir = "tests/{!s}_tests".format(sys.argv[1])
-    acepeted_languages = ["python3","C++","C#"]
+    acepeted_languages = ["python3","C++"]
 
     json_file = read_git_url_json()
 
@@ -103,14 +124,17 @@ if __name__ == '__main__':
         language = student["language"]
         args = student["run_args"]
         args = args.split()
+        compile_args = ""
         
         if (language not in acepeted_languages):
             raise Exception("language {!s} is not a acepeted language!".format(language))
 
-        if (language != "python3"): #tmp perigoso
-            continue
+        if (language != "python3"): # pegar argumento de compilação
+            compile_args = student["compile_args"]
+            compile_args = compile_args.split()
+            compile_args.append("-w") #supress warnings
         
         if (not os.path.exists("reports/{!s}.txt".format(p))):
             print(p)
-            error = test_main(p,test_dir,language,args)
+            error = test_main(p,test_dir,language,args,compile_args)
             print("algum erro?: ",error)
